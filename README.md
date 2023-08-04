2963d58980c803d49519689504460a15a07aedd6
Olivier <olivier.jarrosson@gmaill.com>
2023-08-04 09:33:31 +0200

    coments & read me

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # binarySearchTree
+Binary a binary search tree consists of a branch structure, where each branch has a value, and 2 branch options.
+
+The insert function, insterts on the left side smaller & equal values to the value of the branch
+
+
+insert (Tree, number)->     Number= value to insert in the tree
+                            Tree= The tree the value is going to be inserted in
--- binarySearchTree.fs
+++ binarySearchTree.fs
@@ -3,17 +3,23 @@ type Branch =
     |None
 and Tree ={left:Branch;value:float;right:Branch}    // and, so it can refear to branch
 
-let x={left=None;value=2;right=None};
+let x={left=None;value=3;right=None};
 let y={left=Tree {left=None;value=1;right=None};value=2;right=None};
+
+
+// Insert function will add value to the tree
 
 let rec insert ( tree:Tree, num:float):Tree=
     match tree with
     | {left=l;value=v;right=r} ->
-        if v>=num then {left=Tree(insertHelp (l,num));value=v;right=r} 
+        if v>=num 
+        then {left=Tree(insertHelp (l,num));value=v;right=r} 
         else {left=l;value=v;right=Tree(insertHelp (r,num))}
+        
+//Helper function will filter the cases where there is no more tree going out, and insert the value directly
 and insertHelp (branch: Branch, num:float):Tree=
     match branch with
-    |None-> {left=None;value=num;right=None}
-    |Tree t-> insert (t, num)
+    |None-> {left=None;value=num;right=None}//In case of no branch, create one with no branches
+    |Tree t-> insert (t, num)// If thre is branches, keep searching where the number belongs
     
-let z = insert (y,3.0)
+let z = insert (x,2.0)
